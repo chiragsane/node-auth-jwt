5e427e28daeb5fa31007597acd0edd84fe008797
Chirag Sane <chiragsane333@gmail.com>
2017-10-03 21:50:01 +0530

    sucessful implementation @https://scotch.io/tutorials/authenticate-a-node-js-api-with-json-web-tokens

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'secret': 'node-auth-jwt-db-mlab',
+    // 'database': 'mongodb://node-auth-jwt-db-user:node-auth-jwt-db-password@ds155934.mlab.com:55934/node-auth-jwt-db'
+    'database': 'mongodb://localhost:27017/node-auth-jwt-db'
+};
---
+++ server.js
@@ -0,0 +1,98 @@
+// =======================
+// get the packages we need ============
+// =======================
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./config'); // get our config file
+var User = require('./app/models/user'); // get our mongoose model
+
+// =======================
+// configuration =========
+// =======================
+var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
+mongoose.connect(config.database); // connect to database
+app.set('superSecret', config.secret); // secret variable
+
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
+// =======================
+// routes ================
+// =======================
+// basic route
+app.get('/', function(req, res) {
+    res.send('Hello! The API is at http://localhost:' + port + '/api');
+});
+
+// API ROUTES -------------------
+// we'll get to these in a second
+app.get('/setup', function(req, res) {
+    // create a sample user
+    var nick = new User({
+        name: 'Chirag Sane',
+        password: 'password',
+        admin: true
+    });
+
+    // save the sample user
+    nick.save(function(err) {
+        if (err) throw err;
+
+        console.log('User saved successfully');
+        res.json({ success: true });
+    });
+});
+// get an instance of the router for api routes
+var apiRoutes = express.Router();
+app.use('/api', apiRoutes);
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+apiRoutes.post('/authenticate', function(req, res) {
+
+    // find the user
+    User.findOne({
+        name: req.body.name
+    }, function(err, user) {
+
+        if (err) throw err;
+
+        if (!user) {
+            res.json({ success: false, message: 'Authentication failed. User not found.' });
+        } else if (user) {
+
+            // check if password matches
+            if (user.password != req.body.password) {
+                res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+            } else {
+
+                // if user is found and password is right
+                // create a token
+                var token = jwt.sign({ data: user }, app.get('superSecret'), {
+                    expiresIn: 1440 // expires in 24 hours
+                });
+
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
+
+        }
+
+    });
+});
+// =======================
+// start the server ======
+// =======================
+app.listen(port);
+console.log('Magic happens at http://localhost:' + port);
